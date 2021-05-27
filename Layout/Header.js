6f5cc24857d8605608a46eabd48b9e54ea9b02c7
Sandro Bochorishvili <Sanideth@gmail.com>
2021-05-27 22:25:50 +0400

    added multiple states for the user registration popup input handling

--- Layout/Header.js
+++ Layout/Header.js
@@ -1,5 +1,38 @@
+import { useState } from "react";
 import Icon from "../components/Icon";
+import RegistrationPopup from "../components/RegistrationPopup";
+
 const Header = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setUserPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [isUserClicked, setIsUserClicked] = useState(false);
+
+  const handleUserNameChange = (e) => {
+    setUserName(e.target.value);
+  };
+  const handlePasswordChange = (e) => {
+    setUserPassword(e.target.value);
+  };
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleUserClick = () => {
+    setIsUserClicked(!isUserClicked);
+  };
+
+  const renderPopup = isUserClicked ? (
+    <RegistrationPopup
+      user={true}
+      userNameValue={userName}
+      passwordValue={password}
+      emailValue={email}
+      onUserNameChange={handleUserNameChange}
+      onPasswordChange={handlePasswordChange}
+      onEmailChange={handleEmailChange}
+    />
+  ) : null;
   return (
     <header className="header">
       <div className="header__social-media">
@@ -7,7 +40,8 @@ const Header = () => {
           <Icon icon="search" />
         </div>
         <div className="header__icon-container">
-          <Icon icon="user" />
+          <Icon icon="user" onUserClick={handleUserClick} />
+          {renderPopup}
         </div>
       </div>
     </header>

a8230ebe7b144ee6142eb764e1051156ba9a756e
Sandro Bochorishvili <Sanideth@gmail.com>
2021-05-27 20:04:50 +0400

    changed props to icon component to render icons

--- Layout/Header.js
+++ Layout/Header.js
@@ -1,12 +1,13 @@
+import Icon from "../components/Icon";
 const Header = () => {
   return (
     <header className="header">
       <div className="header__social-media">
         <div className="header__icon-container">
-          {/* <image></image> */}
+          <Icon icon="search" />
         </div>
         <div className="header__icon-container">
-          {/* <image></image> */}
+          <Icon icon="user" />
         </div>
       </div>
     </header>

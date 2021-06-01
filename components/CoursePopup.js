81dc4e542bdad8b014ce1c4e6474cb9cb96b5118
Sandro Bochorishvili <Sanideth@gmail.com>
2021-06-01 15:35:11 +0400

    added some span elements for text read more logic

--- components/CoursePopup.js
+++ components/CoursePopup.js
@@ -2,13 +2,29 @@ import Heading from "../components/Heading";
 import Paragraph from "../components/Paragraph";
 import Button from "../components/Button";
 import Image from "next/image";
+import { useState } from "react";
 
 const CoursePopup = (props) => {
+  const [courseInfoSpanIsOpen, setCourseInfoSpanIsOpen] = useState(false);
+  const [courseLearnWhatSpanIsOpen, setCourseLearnWhatSpanIsOpen] =useState(false);
   const handleOnClick = (e) => {
     if (e.target.classList == "course-popup") {
       props.setIsCardClicked(false);
     }
   };
+
+  const renderCourseInfoSpanClassName = courseInfoSpanIsOpen ? "span-open" : "";
+  const renderCourseInfoButtonClassName = courseInfoSpanIsOpen ? "span-button-close" : "";
+  const renderCourseLearnWhatSpanClassName= courseLearnWhatSpanIsOpen ? "span-open" : "";
+  const renderCourseLearnWhatButtonClassName = courseLearnWhatSpanIsOpen ? "span-button-close" : "";
+
+
+  const handleCourseInfoClick = () => {
+    setCourseInfoSpanIsOpen(!courseInfoSpanIsOpen)
+  }
+  const handleCourseLearnWhatClick = () => {
+    setCourseLearnWhatSpanIsOpen(!courseLearnWhatSpanIsOpen);
+  }
   return (
     <div className="course-popup" onClick={handleOnClick}>
       <div className="course-popup__box">
@@ -23,7 +39,7 @@ const CoursePopup = (props) => {
             className={props.aboutCourseHeadingClassName}
             text={props.aboutCourseHeadingText}
           />
-          <Paragraph text={props.courseInfoText} className="paragraph-popup" />
+          <p className="paragraph-popup">{props.courseInfoTextLess}<span className={`course-popup__span-button ${renderCourseInfoButtonClassName}`} onClick={handleCourseInfoClick}>&nbsp;{props.spanButton}</span> <span className={`course-popup__span-more-text ${renderCourseInfoSpanClassName}`}>{props.courseInfoTextMore}</span></p>
         </div>
         <div className="course-popup__info">
           <Heading
@@ -31,10 +47,10 @@ const CoursePopup = (props) => {
             className={props.aboutCourseHeadingClassName}
             text={props.courseLearnWhatHeadingText}
           />
-          <Paragraph
-            text={props.courseLearnWhatText}
-            className="paragraph-popup"
-          />
+          <p className="paragraph-popup">{props.courseLearnWhatTextLess}<span className={`course-popup__span-button ${renderCourseLearnWhatButtonClassName}`} onClick={handleCourseLearnWhatClick}>&nbsp;{props.spanButton}</span> <span className={`course-popup__span-more-text ${renderCourseLearnWhatSpanClassName}`}>{props.courseLearnWhatTextMore}</span></p>
+            
+            
+          
         </div>
         <div className="course-popup__info">
           <Paragraph className={props.priceClassName} text={props.priceText} />

0e7fc2136c270b7dcf7ad7a5dd916e734b527929
Sandro Bochorishvili <Sanideth@gmail.com>
2021-06-02 21:13:30 +0400

    added body dynamic class removal logic, to unfreeze body when popup closes

--- components/CoursePopup.js
+++ components/CoursePopup.js
@@ -6,55 +6,88 @@ import { useState } from "react";
 
 const CoursePopup = (props) => {
   const [courseInfoSpanIsOpen, setCourseInfoSpanIsOpen] = useState(false);
-  const [courseLearnWhatSpanIsOpen, setCourseLearnWhatSpanIsOpen] =useState(false);
+  const [courseLearnWhatSpanIsOpen, setCourseLearnWhatSpanIsOpen] =
+    useState(false);
   const handleOnClick = (e) => {
     if (e.target.classList == "course-popup") {
       props.setIsCardClicked(false);
+      document.body.classList.remove("fixed");
     }
   };
 
   const renderCourseInfoSpanClassName = courseInfoSpanIsOpen ? "span-open" : "";
-  const renderCourseInfoButtonClassName = courseInfoSpanIsOpen ? "span-button-close" : "";
-  const renderCourseLearnWhatSpanClassName= courseLearnWhatSpanIsOpen ? "span-open" : "";
-  const renderCourseLearnWhatButtonClassName = courseLearnWhatSpanIsOpen ? "span-button-close" : "";
-
+  const renderCourseInfoButtonClassName = courseInfoSpanIsOpen
+    ? "span-button-close"
+    : "";
+  const renderCourseLearnWhatSpanClassName = courseLearnWhatSpanIsOpen
+    ? "span-open"
+    : "";
+  const renderCourseLearnWhatButtonClassName = courseLearnWhatSpanIsOpen
+    ? "span-button-close"
+    : "";
 
   const handleCourseInfoClick = () => {
-    setCourseInfoSpanIsOpen(!courseInfoSpanIsOpen)
-  }
+    setCourseInfoSpanIsOpen(!courseInfoSpanIsOpen);
+  };
   const handleCourseLearnWhatClick = () => {
     setCourseLearnWhatSpanIsOpen(!courseLearnWhatSpanIsOpen);
-  }
+  };
   return (
     <div className="course-popup" onClick={handleOnClick}>
       <div className="course-popup__box">
         <div className="course-popup__main-heading-container">
           <Heading
-          headingTag={props.headingTag}
-          className={props.headingClassName}
-          text={props.headingText}
-        />
+            headingTag={props.headingTag}
+            className={props.headingClassName}
+            text={props.headingText}
+          />
         </div>
-        
+
         <div className="course-popup__info">
           <div className="course-popup__secondary-heading-container">
-          <Heading
-            headingTag={props.aboutCourseHeadingTag}
-            className={props.aboutCourseHeadingClassName}
-            text={props.aboutCourseHeadingText}
-          />
+            <Heading
+              headingTag={props.aboutCourseHeadingTag}
+              className={props.aboutCourseHeadingClassName}
+              text={props.aboutCourseHeadingText}
+            />
           </div>
-          <p className="course-popup__paragraph">{props.courseInfoTextLess}<span className={`course-popup__span-button ${renderCourseInfoButtonClassName}`} onClick={handleCourseInfoClick}>{props.spanButton}</span> <span className={`course-popup__span-more-text ${renderCourseInfoSpanClassName}`}>{props.courseInfoTextMore}</span></p>
+          <p className="course-popup__paragraph">
+            {props.courseInfoTextLess}
+            <span
+              className={`course-popup__span-button ${renderCourseInfoButtonClassName}`}
+              onClick={handleCourseInfoClick}
+            >
+              {props.spanButton}
+            </span>{" "}
+            <span
+              className={`course-popup__span-more-text ${renderCourseInfoSpanClassName}`}
+            >
+              {props.courseInfoTextMore}
+            </span>
+          </p>
         </div>
         <div className="course-popup__info">
-        <div className="course-popup__secondary-heading-container">
-          <Heading
-            headingTag={props.aboutCourseHeadingTag}
-            className={props.aboutCourseHeadingClassName}
-            text={props.courseLearnWhatHeadingText}
-          />
+          <div className="course-popup__secondary-heading-container">
+            <Heading
+              headingTag={props.aboutCourseHeadingTag}
+              className={props.aboutCourseHeadingClassName}
+              text={props.courseLearnWhatHeadingText}
+            />
           </div>
-          <p className="course-popup__paragraph">{props.courseLearnWhatTextLess}<span className={`course-popup__span-button ${renderCourseLearnWhatButtonClassName}`} onClick={handleCourseLearnWhatClick}>{props.spanButton}</span> <span className={`course-popup__span-more-text ${renderCourseLearnWhatSpanClassName}`}>{props.courseLearnWhatTextMore}</span></p>
+          <p className="course-popup__paragraph">
+            {props.courseLearnWhatTextLess}
+            <span
+              className={`course-popup__span-button ${renderCourseLearnWhatButtonClassName}`}
+              onClick={handleCourseLearnWhatClick}
+            >
+              {props.spanButton}
+            </span>{" "}
+            <span
+              className={`course-popup__span-more-text ${renderCourseLearnWhatSpanClassName}`}
+            >
+              {props.courseLearnWhatTextMore}
+            </span>
+          </p>
         </div>
         <div className="course-popup__info">
           <Paragraph className="course-popup__price" text={props.priceText} />

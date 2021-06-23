adab3ea42c1a6703eac5ca407d9a200c02983319
Sandro Bochorishvili <Sanideth@gmail.com>
2021-06-23 20:56:06 +0400

    added conditional render for registration popup

--- components/CoursePopup.js
+++ components/CoursePopup.js
@@ -10,6 +10,8 @@ const CoursePopup = (props) => {
   const [courseInfoSpanIsOpen, setCourseInfoSpanIsOpen] = useState(false);
   const [courseLearnWhatSpanIsOpen, setCourseLearnWhatSpanIsOpen] =
     useState(false);
+  const [isRegistrationButtonClicked, setIsRegistrationButtonClicked] =
+    useState(false);
   const handleOnClick = (e) => {
     if (e.target.classList == "course-popup") {
       props.setIsCardClicked(false);
@@ -21,6 +23,9 @@ const CoursePopup = (props) => {
   };
   const handleCourseLearnWhatClick = () => {
     setCourseLearnWhatSpanIsOpen(!courseLearnWhatSpanIsOpen);
+  };
+  const handleRegistrationButtonClick = () => {
+    setIsRegistrationButtonClicked(!isRegistrationButtonClicked);
   };
 
   const renderCourseInfoSpanClassName = courseInfoSpanIsOpen ? "span-open" : "";
@@ -112,97 +117,153 @@ const CoursePopup = (props) => {
                 />
               </div>
             </div>
-
-            <div className="course-popup__content">
-              <div className="course-popup__text-container">
-                <div className="course-popup__info">
-                  <div className="course-popup__secondary-heading-container">
-                    <Heading
-                      headingTag={props.aboutCourseHeadingTag}
-                      className={props.aboutCourseHeadingClassName}
-                      text={props.aboutCourseHeadingText}
-                    />
+            {!isRegistrationButtonClicked ? (
+              <div className="course-popup__content">
+                <div className="course-popup__text-container">
+                  <div className="course-popup__info">
+                    <div className="course-popup__secondary-heading-container">
+                      <Heading
+                        headingTag={props.aboutCourseHeadingTag}
+                        className={props.aboutCourseHeadingClassName}
+                        text={props.aboutCourseHeadingText}
+                      />
+                    </div>
+                    <p className="course-popup__paragraph">
+                      {props.courseInfoTextLess}
+                      <span
+                        className={`course-popup__span-button ${renderCourseInfoButtonClassName}`}
+                        onClick={handleCourseInfoClick}
+                      >
+                        {props.spanMoreButton}
+                      </span>
+                      <span
+                        className={`course-popup__span-more-text ${renderCourseInfoSpanClassName}`}
+                      >
+                        {props.courseInfoTextMore}
+                        {renderCourseInfoLessButton}
+                      </span>
+                    </p>
                   </div>
-                  <p className="course-popup__paragraph">
-                    {props.courseInfoTextLess}
-                    <span
-                      className={`course-popup__span-button ${renderCourseInfoButtonClassName}`}
-                      onClick={handleCourseInfoClick}
-                    >
-                      {props.spanMoreButton}
-                    </span>
-                    <span
-                      className={`course-popup__span-more-text ${renderCourseInfoSpanClassName}`}
-                    >
-                      {props.courseInfoTextMore}
-                      {renderCourseInfoLessButton}
-                    </span>
-                  </p>
+                  <div className="course-popup__info">
+                    <div className="course-popup__secondary-heading-container">
+                      <Heading
+                        headingTag={props.aboutCourseHeadingTag}
+                        className={props.aboutCourseHeadingClassName}
+                        text={props.courseLearnWhatHeadingText}
+                      />
+                    </div>
+                    <p className="course-popup__paragraph">
+                      {props.courseLearnWhatTextLess}
+                      <span
+                        className={`course-popup__span-button ${renderCourseLearnWhatButtonClassName}`}
+                        onClick={handleCourseLearnWhatClick}
+                      >
+                        {props.spanMoreButton}
+                      </span>
+                      <span
+                        className={`course-popup__span-more-text ${renderCourseLearnWhatSpanClassName}`}
+                      >
+                        {props.courseLearnWhatTextMore}
+                        {renderCourseLearnWhatLessButton}
+                      </span>
+                    </p>
+                  </div>
                 </div>
-                <div className="course-popup__info">
-                  <div className="course-popup__secondary-heading-container">
-                    <Heading
-                      headingTag={props.aboutCourseHeadingTag}
-                      className={props.aboutCourseHeadingClassName}
-                      text={props.courseLearnWhatHeadingText}
+                <div className="course-popup__mentor-container">
+                  <div className="course-popup__info">
+                    <Paragraph
+                      className="course-popup__price"
+                      text={props.priceText}
+                    />
+                    <Paragraph
+                      className="course-popup__meeting"
+                      text={props.meetingText}
                     />
                   </div>
-                  <p className="course-popup__paragraph">
-                    {props.courseLearnWhatTextLess}
-                    <span
-                      className={`course-popup__span-button ${renderCourseLearnWhatButtonClassName}`}
-                      onClick={handleCourseLearnWhatClick}
-                    >
-                      {props.spanMoreButton}
-                    </span>
-                    <span
-                      className={`course-popup__span-more-text ${renderCourseLearnWhatSpanClassName}`}
-                    >
-                      {props.courseLearnWhatTextMore}
-                      {renderCourseLearnWhatLessButton}
-                    </span>
-                  </p>
+                  <div className="course-popup__button-registration-container">
+                    <Button
+                      className="btn-course-registration"
+                      text="რეგისტრაცია"
+                      onClick={handleRegistrationButtonClick}
+                    />
+                  </div>
+                  {props.isDesktop ? (
+                    <div className="course-popup__img-container">
+                      <Image
+                        src={props.mentorImgSrc}
+                        height={props.mentorImgHeight}
+                        width={props.mentorImgWidth}
+                        alt={props.mentorImgAlt}
+                        className={props.mentorImgClassName}
+                      />
+                    </div>
+                  ) : null}
+
+                  <div className="course-popup__button-contact-container">
+                    <Button
+                      className="btn-course-contact"
+                      text={`${
+                        props.isDesktop ? "კონტაქტი" : "დაუკავშირდი მენტორს"
+                      }`}
+                    />
+                  </div>
                 </div>
               </div>
-              <div className="course-popup__mentor-container">
-                <div className="course-popup__info">
-                  <Paragraph
-                    className="course-popup__price"
-                    text={props.priceText}
-                  />
-                  <Paragraph
-                    className="course-popup__meeting"
-                    text={props.meetingText}
-                  />
-                </div>
-                <div className="course-popup__button-registration-container">
-                  <Button
-                    className="btn-course-registration"
-                    text="რეგისტრაცია"
-                  />
-                </div>
-                {props.isDesktop ? (
-                  <div className="course-popup__img-container">
-                    <Image
-                      src={props.mentorImgSrc}
-                      height={props.mentorImgHeight}
-                      width={props.mentorImgWidth}
-                      alt={props.mentorImgAlt}
-                      className={props.mentorImgClassName}
+            ) : (
+              <div className="course-popup__registration">
+                <form action="#" className="course-popup__registration-form">
+                  <div className="course-popup__registration-input-field">
+                    <input
+                      type="text"
+                      placeholder="დაწყების თარიღი"
+                      className="course-popup__registration-input course-popup__registration-input--date"
                     />
                   </div>
-                ) : null}
-
-                <div className="course-popup__button-contact-container">
-                  <Button
-                    className="btn-course-contact"
-                    text={`${
-                      props.isDesktop ? "კონტაქტი" : "დაუკავშირდი მენტორს"
-                    }`}
-                  />
-                </div>
+                  <div className="course-popup__registration-input-field">
+                    <select
+                      type="text"
+                      className="course-popup__registration-input course-popup__registration-input--time"
+                      id="time"
+                      name="time"
+                    >
+                      <option value="" disabled selected>
+                        Select your option
+                      </option>
+                      <option value="12 საათი">12 საათი</option>
+                      <option value="14 საათი">14 საათი</option>
+                      <option value="17 საათი">17 საათი</option>
+                      <option value="20 საათი">20 საათი</option>
+                    </select>
+                  </div>
+                  <div className="course-popup__registration-input-field">
+                    <textarea
+                      className="course-popup__registration-input course-popup__registration-input--textarea"
+                      placeholder="შეტყობინება მენტორისთვის"
+                      cols="30"
+                      rows="10"
+                    ></textarea>
+                  </div>
+                  <div className="course-popup__registration-input-field">
+                    <label htmlFor="radio" className="course-popup__radio">
+                      გავეცანი კურსის პირობებს
+                      <input
+                        type="checkbox"
+                        id="radio"
+                        name="radio"
+                        className="course-popup__registration-input course-popup__registration-input--radio"
+                      />
+                      <span className="course-popup__checkmark"></span>
+                    </label>
+                  </div>
+                  <div className="course-popup__registration-button-container">
+                    <Button
+                      className="btn-course-registration"
+                      text="რეგისტრაციის მოთხოვნა"
+                    />
+                  </div>
+                </form>
               </div>
-            </div>
+            )}
           </div>
         </div>
       );
